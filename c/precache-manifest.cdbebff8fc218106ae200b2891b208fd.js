f9d934e1190758ac5cc67d9e7b7f274aec58de69
Amos Haviv <mail@amoshaviv.com>
2020-06-09 01:01:16 +0200

    Fix

---
+++ c/precache-manifest.cdbebff8fc218106ae200b2891b208fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c070b67537633bec075c510c422d9255",
+    "url": "/c/index.html"
+  },
+  {
+    "revision": "be9960d627d35425c955",
+    "url": "/c/static/js/2.e9c242d4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/c/static/js/2.e9c242d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03edd08334369c8219d5",
+    "url": "/c/static/js/main.d3e7ec1e.chunk.js"
+  },
+  {
+    "revision": "45288aa0bf4ec1017237",
+    "url": "/c/static/js/runtime-main.89d9f2fb.js"
+  }
+]);
